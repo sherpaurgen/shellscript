5e574944ae2803264ce4cf7cf333a4dd0860c144
chingchong <norbuurgen@gmail.com>
2016-11-16 23:52:27 +0545

    Update backup-fullsystem.sh

--- backup-fullsystem.sh
+++ backup-fullsystem.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-tar -zvcpf /backups/fullbackup-`date +"%s-%F"`.tar.gz  --exclude=/etc/network/interfaces --exclude=/etc/machine-id --exclude=/etc/network/interfaces --exclude=/etc/udev/rules.d/70-persistent-net.rules  --exclude=/backups --exclude=/proc --exclude=/run --exclude=/etc/network/interfaces --one-file-system /  2> /backups/err.log  1> /backups/backup.log
+tar -zvcpf /backups/fullbackup-`date +"%s-%F"`.tar.gz  
+--exclude=/etc/network/interfaces 
+--exclude=/etc/machine-id 
+--exclude=/etc/network/interfaces 
+--exclude=/etc/udev/rules.d/70-persistent-net.rules  
+--exclude=/backups --exclude=/proc 
+--exclude=/run
+--exclude=/etc/network/interfaces --one-file-system /  2> /backups/err.log  1> /backups/backup.log
