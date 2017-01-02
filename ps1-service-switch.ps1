943bea29f36866637cba27ffb79c8b0e2629b4b9
chingchong <norbuurgen@gmail.com>
2017-01-03 01:14:08 +0545

    Update ps1-service-switch.ps1

--- ps1-service-switch.ps1
+++ ps1-service-switch.ps1
@@ -1,16 +1,18 @@
 #powershell script to failover to services on another
+#service names
 $servicegroup = "snmp","snmptrap"
+#backup server hostname
 $standbyserver_hostname="server2"
 $counter=0
-$global_counter=0
-#check service locally
+#$global_counter=0
+
+#first check services locally
 foreach($service in $servicegroup) {
         $arrService = Get-Service -Name $service
         if ($arrService.Status -ne "Running"){
         $counter=$counter+1
          }
 }
-echo $counter
 
 if($counter -gt 0){
     foreach($job in $servicegroup){
