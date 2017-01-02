0045d429884a49616c98c527277b28a1bf2c692e
chingchong <norbuurgen@gmail.com>
2017-01-03 01:56:49 +0545

    Update ps1-service-switch.ps1

--- ps1-service-switch.ps1
+++ ps1-service-switch.ps1
@@ -10,18 +10,22 @@ $counter=0
 foreach($service in $servicegroup) {
         $arrService = Get-Service -Name $service
         if ($arrService.Status -ne "Running"){
+        #increase counter by 1 if any 1 service is down on primaryserver
         $counter=$counter+1
          }
 }
-
+#action to take if any one service is down on primaryserver
 if($counter -gt 0){
     foreach($job in $servicegroup){
-                 echo "Stopping: $job"
+                 #echo "Stopping: $job"
+                 #Stop all service on array
                  Get-Service -Name $job | Stop-Service
+                 #Start all service listed on array at remote site
                  Get-Service $job -ComputerName $standbyserver_hostname | Start-Service
              } 
 }
 else {
+#if all service are running locally then make sure those are not running on standby server
     foreach($job in $servicegroup){
         Get-Service $job -ComputerName $standbyserver_hostname | Stop-Service
         } 
