d3401d23759137716020d4d342bb24652d8f01e1
chingchong <norbuurgen@gmail.com>
2017-02-03 16:50:47 +0545

    Update mysqlbackup-radacct.sh

--- mysqlbackup-radacct.sh
+++ mysqlbackup-radacct.sh
@@ -2,8 +2,11 @@
 #This script will backup the radacct table on daily basis
 
 #sys_date=`date +%F -d '-1 day'`
+
 #/usr/bin/mysqldump --single-transaction --no-create-db --no-create-info ctradius radacct --where="acctstarttime LIKE '${sys_date}%'" >  /dbbackup/ctradius/radacct-`date +%F -d '-1 day'`.sql
-#Below script is set to run cronjob every sunday to backup weekly radacct from monday to saturday
-sys_date1=`date +%F -d '-7 day'`;
-sys_date2=`date +%F -d '-1 day'`;
+sys_date1="`date +%F -d '-7 day'` 00:00:00";
+sys_date2="`date +%F -d '-1 day'` 23:59:59";
+#echo $sys_date1
+#echo $sys_date2
+#run cronjob every sunday to backup weekly radacct from monday to saturday
 /usr/bin/mysqldump --single-transaction --no-create-db --no-create-info ctradius radacct --where="acctstarttime between '${sys_date1}' and '${sys_date2}'" >  /dbbackup/ctradius/radacct-week-`date +%F -d '-1 day'`.sql
