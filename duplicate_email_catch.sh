f0a6d0f271b7afdc6ac334bf2943762c0003983f
urgen sherpa <norbuurgen@gmail.com>
2017-10-04 22:30:07 +0545

    script to catch duplicate emails from postfixlog

---
+++ duplicate_email_catch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+grep 'status=sent' /var/log/maillog | grep -v "axonadvisers" |  awk -F"[<>]" '{print $2}' | sort | uniq -c |  awk '{ print $1" "$2; }' | sort -nr > /var/log/summary_`date +%F-%H`.txt
+
+grep -wv "1" /var/log/summary_`date +%F-%H`.txt > /var/log/sorted_summary_`date +%F-%H`.txt
+
+echo "This is an automated report. Please see the attached txt file to check if duplicate emails are sent,if file is blank then no duplicates are sent" | /usr/bin/mutt -a "/var/log/sorted_summary_`date +%F-%H`.txt" -e "my_hdr From:system@axonadvisers.com" -s "No reply: Duplicate Summary" --  johndoe@example.com,user101@hubgit.com,user111@football.com
