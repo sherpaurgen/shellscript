880175bbb89e0b60dbcc57c2a4ab5fe5a6f28003
urgen sherpa <norbuurgen@gmail.com>
2017-10-08 14:13:23 +0545

    Update duplicate_email_catch.sh

--- duplicate_email_catch.sh
+++ duplicate_email_catch.sh
@@ -5,7 +5,8 @@ grep -wv "1" /var/log/summary_`date +%F-%H`.txt > /var/log/sorted_summary_`date 
 
 filename=`ls -1t /var/log/sorted_summary_* | head -1`
 filename2='/var/log/tempaddr.txt'
-
+total=`wc -l ${filename}`
+sentmail=`grep "status=sent" /var/log/maillog | grep  -v "axonadviser" | wc -l`
 cut -d' ' -f2 ${filename} > ${filename2}
 echo >> ${filename}
 echo "------Email--------SentTimeStamp-------" >> ${filename}
@@ -14,7 +15,9 @@ for addr in `cat ${filename2}`;
      grep "${addr}" /var/log/maillog | grep 'status=sent' | grep -vE "axonadvis|norbu|brammer" | grep "to=<"  |  awk -F"[<>]" '{print $2"-->"$1}' | awk -F "server" '{ print $1}' >> ${filename}
  done
 sed -i '1i--TimesSent--RcptEmail--' ${filename}
-    
+echo >> ${filename}
+echo "Total Duplicate Emails - ${total}" >> ${filename}
         
 #test email for me only
-echo "This is an automated report. Please see the attached txt file to check if duplicate emails are sent,if file is blank then no duplicates are sent" | /usr/bin/mutt -a "${filename}" -e "my_hdr From:system@aexample.com" -s "No reply: Duplicate Summary" --  abcd@gmail.com
+echo "This is an automated report. Please see the attached txt file to check if duplicate emails are sent,if file is blank then no duplicates are sent. Duplicate emails found total:${total}" | /usr/bin/mutt -a "${filename}" -e "my_hdr From:system@axonadvisers.com" -s "No reply: Duplicate Summary. TotalEmailsSent : ${sentmail} " --  urgen@gmail.com,sherpa@gmail.com
+
